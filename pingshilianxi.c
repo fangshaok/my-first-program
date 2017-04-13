23c4dfce68d0566e6705870ceb9391eab6317a6d
fangshaok <2695843424@qq.com>
2017-04-13 18:20:46 +0800

    Add files via upload

---
+++ pingshilianxi.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()/*
+{
+	int a,b,t,c;
+	printf("请输入两个数\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		a=a;
+		b=b;
+	}
+	else
+	{ 
+		c=a;
+		a=b;
+		b=c;
+	}
+	do{
+		t=a%b;
+		a=b;
+		b=t;
+	}
+	while(b==0);
+	printf("最大公约数为%d\n",a);
+	return 0;
+}// 
+    *//*
+{
+	int i,j,k;
+        printf("这个程序是输出1000以内的完数\n");
+		printf("请输入一个数\n");
+		scanf("%d",&i);
+    for(;i<=1000;i++)//1到1000的循环
+	{   
+		k=0;//k清零
+    for (j=1;j<=i/2;j++)//求因子
+	{
+		if (i%j==0)  //判断是否为因子
+        {
+			k=k+j;//因子之和
+	}
+	}
+        if(k==i)//判断是否为完数
+		{
+           printf("%d\n",k);//输出完数
+		}
+	
+}
+	return 0;
+	
+}*///9
+
